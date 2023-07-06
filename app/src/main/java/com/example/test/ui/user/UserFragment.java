a39ad5ab17c4625aa8efe12915a1814f7fe4fcca
Anthony <tenmo2003@gmail.com>
2023-07-06 21:19:15 +0700

    First commit

---
+++ app/src/main/java/com/example/test/ui/user/UserFragment.java
@@ -0,0 +1,37 @@
+package com.example.test.ui.user;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.test.databinding.FragmentUserBinding;
+
+public class UserFragment extends Fragment {
+
+    private FragmentUserBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        UserViewModel userViewModel =
+                new ViewModelProvider(this).get(UserViewModel.class);
+
+        binding = FragmentUserBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textUser;
+        userViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
