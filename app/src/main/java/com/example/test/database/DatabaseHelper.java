fb4ab77e499e4bff5e4baa0dedc4af1ca10d4dd5
Anthony <tenmo2003@gmail.com>
2023-07-07 16:22:44 +0700

    Added share tab

---
+++ app/src/main/java/com/example/test/database/DatabaseHelper.java
@@ -0,0 +1,81 @@
+package com.example.test.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "chef.db";
+
+    public static final String SQL_CREATE_ENTRIES_USER = "CREATE TABLE user (\n" +
+                                                        "  username TEXT PRIMARY KEY,\n" +
+                                                        "  password TEXT NOT NULL,\n" +
+                                                        "  points INTEGER DEFAULT 0\n" +
+                                                        "); ";
+
+    public static final String SQL_CREATE_ENTRIES_ARTICLE = "CREATE TABLE article (\n" +
+                                                            "  id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                                                            "  publisher TEXT NOT NULL,\n" +
+                                                            "  dish_name TEXT NOT NULL,\n" +
+                                                            "  meal TEXT CHECK(meal IN ('Bữa sáng', 'Bữa trưa', 'Bữa tối', 'Flexible')) NOT NULL,\n" +
+                                                            "  serve_order_class TEXT CHECK(serve_order_class IN ('Món khai vị', 'Món chính', 'Món tráng miệng')) NOT NULL,\n" +
+                                                            "  type TEXT CHECK(type IN ('Món thịt', 'Món hải sản', 'Món chay', 'Món canh', 'Món rau', 'Mì', 'Bún', 'Món cuốn', 'Món xôi', 'Món cơm', 'Món bánh mặn', 'Món bánh ngọt')) NOT NULL,\n" +
+                                                            "  content TEXT NOT NULL,\n" +
+                                                            "  likes INTEGER DEFAULT 0,\n" +
+                                                            "  dislikes INTEGER DEFAULT 0,\n" +
+                                                            "  published_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+                                                            "  FOREIGN KEY(publisher) REFERENCES user(username)\n" +
+                                                            ");";
+
+    public static final String SQL_CREATE_ENTRIES_BOOKMARK = "CREATE TABLE bookmark (\n" +
+                                                            "  user TEXT NOT NULL,\n" +
+                                                            "  article INTEGER NOT NULL,\n" +
+                                                            "  FOREIGN KEY(user) REFERENCES user(username),\n" +
+                                                            "  FOREIGN KEY(article) REFERENCES article(id),\n" +
+                                                            "  PRIMARY KEY(user, article)\n" +
+                                                            ");";
+
+    public static final String SQL_CREATE_ENTRIES_COMMENT = "CREATE TABLE comment (\n" +
+                                                            "  id INTEGER PRIMARY KEY,\n" +
+                                                            "  article_id INTEGER NOT NULL,\n" +
+                                                            "  commenter TEXT NOT NULL,\n" +
+                                                            "  content TEXT NOT NULL,\n" +
+                                                            "  FOREIGN KEY(article_id) REFERENCES article(id),\n" +
+                                                            "  FOREIGN KEY(commenter) REFERENCES user(username)" +
+                                                            ");";
+
+    public static final String SQL_CREATE_ENTRIES_ARTICLE_LIKE = "CREATE TABLE article_like (\n" +
+                                                                "  user TEXT NOT NULL,\n" +
+                                                                "  article INTEGER NOT NULL,\n" +
+                                                                "  PRIMARY KEY(user, article),\n" +
+                                                                "  FOREIGN KEY(user) REFERENCES user(username),\n" +
+                                                                "  FOREIGN KEY(article) REFERENCES article(id)\n" +
+                                                                ");";
+
+    public static final int DB_VERSION = 1;
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES_USER);
+
+        db.execSQL(SQL_CREATE_ENTRIES_ARTICLE);
+
+        db.execSQL(SQL_CREATE_ENTRIES_BOOKMARK);
+
+        db.execSQL(SQL_CREATE_ENTRIES_COMMENT);
+
+        db.execSQL(SQL_CREATE_ENTRIES_ARTICLE_LIKE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/example/test/ui/share/ShareFragment.java
@@ -0,0 +1,38 @@
+package com.example.test.ui.share;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.test.databinding.FragmentSearchBinding;
+import com.example.test.databinding.FragmentShareBinding;
+import com.example.test.ui.search.SearchViewModel;
+
+public class ShareFragment extends Fragment {
+    private FragmentShareBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ShareViewModel shareViewModel =
+                new ViewModelProvider(this).get(ShareViewModel.class);
+
+        binding = FragmentShareBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textShare;
+        shareViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
