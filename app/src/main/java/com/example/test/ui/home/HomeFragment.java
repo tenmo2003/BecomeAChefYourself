ce3875aa8d36a4994c90634276abd7d94d007f0e
ThuanLee <21020409@vnu.edu.vn>
2023-07-12 09:00:23 +0700

    update fragment home

--- app/src/main/java/com/example/test/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/test/ui/home/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.test.ui.home;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,24 +12,35 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import com.example.test.R;
 import com.example.test.adapters.RecipeListAdapter;
+import com.example.test.adapters.RecommendRecipeAdapter;
 import com.example.test.databinding.FragmentHomeBinding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     RecipeListAdapter recipeListAdapter;
-    RecyclerView recyclerView;
+    RecommendRecipeAdapter recommendRecipeAdapter;
+    RecyclerView recipeListView, recommendRecipeView;
     SearchView searchView;
+    Timer timer;
+    TimerTask timerTask;
+    int position = Integer.MAX_VALUE / 2;
     List<String> list = Arrays.asList("Thịt bò", "Thịt gà", "Cơm rang",
             "Xôi", "Bánh cuốn", "Chả cá", "Canh rau", "Khoai tây", "Chuối", "Pizza");
 
@@ -38,14 +50,28 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        recyclerView = view.findViewById(R.id.recipe_list);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        //Recycler display recommend recipe
+        recommendRecipeView = view.findViewById(R.id.recommend_recipe_list);
+        recommendRecipeView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        recommendRecipeAdapter = new RecommendRecipeAdapter();
+        recommendRecipeView.setAdapter(recommendRecipeAdapter);
+
+        recommendRecipeView.scrollToPosition(position);
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(recommendRecipeView);
+        recommendRecipeView.smoothScrollBy(100, 0);
+
+        //Recycler display grid recipe
+        recipeListView = view.findViewById(R.id.recipe_list);
+        recipeListView.setHasFixedSize(true);
+        recipeListView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
 
         recipeListAdapter = new RecipeListAdapter();
         recipeListAdapter.setDish_names(list);
-        recyclerView.setAdapter(recipeListAdapter);
+        recipeListView.setAdapter(recipeListAdapter);
 
+        //Search bar
         searchView = view.findViewById(R.id.search_view);
         searchView.clearFocus();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -64,7 +90,20 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
-    public void filterList(String text) {
+    private void autoScroll() {
+        if (timer == null && timerTask == null) {
+            timer = new Timer();
+            timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    recommendRecipeView.scrollToPosition(position++);
+                }
+            };
+            timer.schedule(timerTask, 3000, 3000);
+        }
+    }
+
+    private void filterList(String text) {
         System.out.print(text);
         List<String> filteredList = new ArrayList<>();
         for (String dish_name: list) {
